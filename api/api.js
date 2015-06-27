a357711fad6ce8691da2e99317abdb3fabce2244
Markus Török <markus.toeroek@gmail.com>
2015-06-27 14:13:08 +0200

    saving a hotel through api possible

--- api/api.js
+++ api/api.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose   = require("mongoose");
+mongoose.connect("localhost/travelmate");
+
+var Hotel = require("./models/hotel");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -11,6 +15,29 @@ var router = express.Router();
 router.get("/", function(req, res) {
     res.json({ message: "Welcome to the TravelMate API!" });
 });
+
+router.route('/hotels')
+    
+    .post(function(req, res) {
+        var hotel = new Hotel(req.body);
+
+        hotel.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: "hotel " + hotel.name + " created!" });
+        });
+
+    })
+
+    .get(function(req, res) {
+        Hotel.find(function(err, hotels) {
+            if (err)
+                res.send(err);
+
+            res.json(hotels);
+        });
+    });
 
 app.use("/api", router);
 
---
+++ api/models/hotel.js
@@ -0,0 +1,64 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var HotelSchema   = new Schema({
+    "name": String,
+    "stars": Number,
+    "address": {
+        "street": String,
+        "city": String,
+        "zip": String,
+        "country": String
+    },
+    "breakfastTypes": Array,
+    "coordinates": {
+        "lat": Number,
+        "lng": Number
+    },
+    "suitableFor": {
+        "Partygänger": Boolean,
+        "Geschäftsleute": Boolean,
+        "Singles": Boolean,
+        "Familien": Boolean,
+        "Hochzeitsreisende": Boolean
+    },
+    "features": {
+        "pool": Boolean,
+        "parking": Boolean,
+        "sauna": Boolean
+    },
+    "distancesInKm": {
+        "aiprort": Number,
+        "cityCentre": Number
+    },
+    "rooms": [
+        {
+            "id": String,
+            "numberOfAdults": Number,
+            "costPerNightInEuro": Number,
+            "roomNumber": Number,
+            "floor": Number,
+            "features": {
+                "wifi": Boolean,
+                "minibar": Boolean
+            },
+            "availability": {
+                "from": { type: Date, default: Date.now },
+                "to": { type: Date, default: Date.now }
+            },
+            "booked": Boolean,
+            "bookingDates": [
+                {
+                    "from": { type: Date, default: Date.now },
+                    "to": { type: Date, default: Date.now }
+                },
+                {
+                    "from": { type: Date, default: Date.now },
+                    "to": { type: Date, default: Date.now }
+                }
+            ]
+        }
+    ]
+});
+
+module.exports = mongoose.model('Hotel', HotelSchema);
